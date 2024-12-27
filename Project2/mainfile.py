a24afe47d0050414d7f9f1323024b4c69a66de08
traininNG1 <jesna.shenil@xminds.com>
2024-12-27 18:16:07 +0530

    updated fastapi codes

--- Project2/mainfile.py
+++ Project2/mainfile.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI,Query,Path
-from pydantic import BaseModel
+from fastapi import FastAPI,Query,Path, Body
+from pydantic import BaseModel,Field,EmailStr
 from typing import Optional
 
 app = FastAPI()
@@ -125,20 +125,20 @@ def userlist121(user_id : str =Path(..., regex="^[A-Z0-9]{7}$")):
 # simple request body ( video 7)
 # need to import FastAPI & BaseModel
 
-class Offer(BaseModel):
+""" class Offer(BaseModel):
     name : str
     price : float
     is_offer : bool = False
 
 @app.post("/items")
 def offer134(item:  Offer):
-    return {"item" : item} 
+    return {"item" : item}  """
 
 # ex2 MIXING PATH, QUERY & BODY PARAMETERS
 # here import FastAPI & BaseModel
 
 
-class Purchase(BaseModel):
+""" class Purchase(BaseModel):
     name : str
     price : float
 
@@ -146,12 +146,12 @@ class Purchase(BaseModel):
 def item146(item : Purchase, item_id : int, q : str | None=None):
     return{
         "itemid": item_id, "item": item , "query":  q
-    }
+    } """
 
 # MULTIPLE REQUEST BODIES
 # here import FastAPI & BaseModel
 
-class User(BaseModel):
+""" class User(BaseModel):
     name : str
     email : str
     address : str
@@ -163,5 +163,107 @@ class Order(BaseModel):
 @app.post("/users/{user_id}")
 def user164( user_details : User, oredr_details : Order):
     return{"user details": user_details, "order details": oredr_details}
+ """
+# FIELD FOR REQUEST BODY
+""" class Item(BaseModel):
+    name : str
+    description : str = None
+    price : float
+    tax : float = None
+
+@app.post("/items")
+def items175(sell : Item):
+    total_price = sell.price + (sell.tax if sell.tax else 0)
+    return { "name": sell.name, "price": sell.price, "tax": sell.tax, "Total_amnt": total_price} """
+
+#NESTED  DATA IN REQUEST BODY
+ #Q1.  create a scenario where we have a nested structure: a User has a Profile, and the Profile includes an address.
+
+""" class Address(BaseModel):
+    street : str
+    city : str
+    pincode : int
+
+class Profile(BaseModel):
+    bio : str
+    email : str
+    address_deatils : Address
+class User(BaseModel):
+    name: str
+    phn : int
+    profile_details : Profile
+@app.post("/users")
+def user_detilas(user1 : User):
+    return {"message": "Details updated successfully!...", "User_details" : user1} """
+
+# video part 10  DECLARING REQUEST EXAMPLE DATA
+#from fastapi import FastAPI
+#from pydantic import BaseModel, Field
+
+# using field parameter
+""" class Itempurch(BaseModel):
+    name : str = Field(...,example= "tv")
+    price : int = Field(...,example="25000")
+    description : str  = Field(None, example="sony television")
+    in_stock : bool = Field(..., example= True)
+
+@app.post("/items")
+def item210(item: Itempurch):
+    return item """
+
+                #same using body parameter
+#from fastapi import FastAPI
+#from pydantic import BaseModel, Body
+""" class Itempurch(BaseModel):
+    name : str = Body(...,example= "tv")
+    price : int = Body(...,example="25000")
+    description : str  = Body(None, example="sony television")
+    in_stock : bool = Body(..., example= True)
+
+@app.post("/items")
+def item210(item: Itempurch):
+    return item """
+
+        #using schema-extra  for full model examples
+""" class Itempurch(BaseModel):
+    name : str 
+    price : int
+    description : str
+    in_stock : bool 
+
+class config :
+    schema_extra = {
+        "example":{
+            "name":  "laptop",
+            "price" : 78999.80,
+            "description" : "hello got item",
+            "in_stock" :  True,
+        }
+    }
+
+@app.post("/items")
+def item210(item: Itempurch):
+    return item """
+
+
+# Emailstr --> for to validate email
+#import Emailstr
+
+class User(BaseModel):
+    email : EmailStr
+
+@app.post("/users")
+def user12(user1 : User):
+    return {"email" : user1.email}
+
+""" 
+class User(BaseModel):
+    name: str
+    email: EmailStr
+
+@app.post("/users/")
+async def create_user(user: User):
+    return {"name": user.name, "email": user.email} """
+
 
 
